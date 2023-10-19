b2651dade93b4c2a12f0c064566fb6e190451838
Reub Vega <126991341+REUBATCODE@users.noreply.github.com>
2023-10-19 16:57:17 -0700

    Conversiones completas
    Signed-off-by: Reub Vega <126991341+REUBATCODE@users.noreply.github.com>

--- app/src/main/java/vega/odyssey/convertidortemperatura/MainActivity.kt
+++ app/src/main/java/vega/odyssey/convertidortemperatura/MainActivity.kt
@@ -1,16 +1,23 @@
 package vega.odyssey.convertidortemperatura
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var conversion : Int = 0
+    private var value : String = ""
+    private var result: Double = 0.0
+    private lateinit var total: TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         val options = resources.getStringArray(R.array.convert_options)
         val spinner =findViewById<Spinner>(R.id.spinTemp)
         val button = findViewById<Button>(R.id.btnConvert)
+        var temp = findViewById<EditText>(R.id.editTemperature)
+        total = findViewById(R.id.txtResult)
 
         if(spinner != null){
             val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
@@ -31,9 +40,10 @@ class MainActivity : AppCompatActivity() {
                     parent: AdapterView<*>?,
                     view: View?, position: Int, id: Long
                 ){
+                    conversion = position
                     Toast.makeText(
                         this@MainActivity,
-                        "Opción seleccionada",
+                        "Opción seleccionada "+position,
                         Toast.LENGTH_SHORT
                     ).show()
 
@@ -45,6 +55,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             button.setOnClickListener{
+                value = temp.text.toString()
                 converter(conversion)
             }
         }
@@ -58,21 +69,44 @@ class MainActivity : AppCompatActivity() {
         when(convert){
             0 -> {
                 //ºC to ºF
+                Toast.makeText(this, "ºC to ºF",Toast.LENGTH_SHORT).show()
+                result = ((value.toDouble()*9/5+32))
+                total.text = result.toString()
             }
             1 -> {
                 //ºC to ºK
+                Toast.makeText(this, "ºC to ºK",Toast.LENGTH_SHORT).show()
+                result = (value.toDouble()+273.15)
+                total.text = result.toString()
+
             }
             2 -> {
                 //ºF to ºC
+                Toast.makeText(this, "ºF to ºC",Toast.LENGTH_SHORT).show()
+                result = ((value.toDouble()-32)/1.8)
+                total.text = result.toString()
+
             }
             3 -> {
                 //ºF to ºK
+                Toast.makeText(this, "ºF to ºK",Toast.LENGTH_SHORT).show()
+                result = ((value.toDouble()-32)*5/9+273.15)
+                total.text = result.toString()
+
             }
             4 -> {
                 //ºK to ºC
+                Toast.makeText(this, "ºK to ºC",Toast.LENGTH_SHORT).show()
+                result = (value.toDouble()-273.15)
+                total.text = result.toString()
+
             }
             5 -> {
                 //ºK to ºF
+                Toast.makeText(this, "ºK to ºF",Toast.LENGTH_SHORT).show()
+                result = ((value.toDouble()-273.15)*9/5+32)
+                total.text = result.toString()
+
             }
         }
     }
